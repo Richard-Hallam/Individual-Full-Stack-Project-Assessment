85e09ab7cd9991c2f1e2a94f67ea1d3e53e6013f
Richard Hallam <richardhallam25@gmail.com>
2024-12-05 11:10:20 +0000

    Added view for transaction list

--- expensetracker/urls.py
+++ expensetracker/urls.py
@@ -16,18 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-<<<<<<< HEAD
 from home.views import home_page
+from transactions.views import transactions_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_page, name='home')
-=======
-from home import views
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', home, name='home')
->>>>>>> d8c60df (added home.html)
+    path('home/', home_page, name='home'),
+    path('transactions/', transactions_list, name='transactions'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+def transactions_list(request):
+    template = loader.get_template('transaction_list.html')
+    return HttpResponse(template.render())
