d52c7e54947ae03e3c1e507c684bff9a3dad58aa
Richard Hallam <richardhallam25@gmail.com>
2024-12-05 10:14:44 +0000

    fixed import error in urls.py

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 
-def home_test(request):
+def home_page(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
