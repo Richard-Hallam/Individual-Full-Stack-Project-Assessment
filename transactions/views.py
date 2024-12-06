e8a25bf916289774ce53198f6d864ede92fdaa00
Richard Hallam <richardhallam25@gmail.com>
2024-12-06 10:36:19 +0000

    fixed form for adding new transaction and created message for succsessful writing of  data

--- transactions/views.py
+++ transactions/views.py
@@ -2,13 +2,16 @@ from django.template import loader
 from .forms import TransactionForm
 from django.shortcuts import render
 from .models import Transaction
+from django.contrib import messages
 
 def transactions_list(request):
     if request.method == 'POST':
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('transactions_list')
+        form = TransactionForm(data=request.POST)
+        form.save()
+        messages.add_message(
+            request, messages.SUCCESS,
+            'transaction submitted succsessfully'
+        )
     else:
         form = TransactionForm()
     transactions = Transaction.objects.all()
