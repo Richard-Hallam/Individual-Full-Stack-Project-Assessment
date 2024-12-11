7cbaf99ca4cfd45c6d373b6a79ce1a5312559e67
Richard Hallam <richardhallam25@gmail.com>
2024-12-11 15:12:29 +0000

    added edit and delete button to transaction list

--- transactions/views.py
+++ transactions/views.py
@@ -1,6 +1,6 @@
 from django.template import loader
 from .forms import TransactionForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transaction
 from django.contrib import messages
 from django.http import HttpResponse
@@ -14,12 +14,14 @@ def transactions_list(request):
             request, messages.SUCCESS,
             'transaction submitted succsessfully'
         )
-        return render(request, 'transactions/transaction_list.html')
+        #return render(request, 'transactions/transaction_list.html')
+        return(redirect('transactions_list'))
     else:
         form = TransactionForm()
+        transactions = Transaction.objects.all()
         context = {'form': form}
-        return render(request, 'transactions/transaction_list.html', context)
-
+        #return render(request, 'transactions/transaction_list.html', context)
+        return render(request, 'transactions/transaction_list.html', {'transactions': transactions, 'form': form})
 
 
 def edit_transaction(request, transaction_id):
@@ -28,14 +30,16 @@ def edit_transaction(request, transaction_id):
         form = TransactionForm(request.POST, instance=transaction)
         if form.is_valid():
             form.save()
-            return redirect('transactions/transactions_list')
+            transactions = Transaction.objects.all()
+            return redirect('transactions/transactions_list', {'transactions' : transactions })
     else:
         form = TransactionForm(instance=transaction)
-    return render(request, 'transactions/transactions_edit.html', {'form': form})
+        transactions = Transaction.objects.all()
+    return render(request, 'transactions/transactions_edit.html', {'transactions': transactions, 'form': form})
 
 def delete_transaction(request, transaction_id):
     transaction = get_object_or_404(Transaction, pk=transaction_id)
     if request.method == 'POST':
         transaction.delete()
         return redirect('transactions_list')
-    return render(request, 'transactions/delete_transaction.html', {'transaction': transaction})
+    return render(request, 'transactions/delete_transaction.html', {'transactions': transaction})
