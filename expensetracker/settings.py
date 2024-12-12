5e2a7f406d443818aa35bfa81361b6f5ff00b24d
Richard Hallam <richardhallam25@gmail.com>
2024-12-12 13:26:59 +0000

    removed commented out code

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -110,15 +110,6 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
