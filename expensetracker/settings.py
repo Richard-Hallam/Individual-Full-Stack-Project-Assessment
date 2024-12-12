f26f608a661631418e811500b35adbb7db9a86eb
Richard Hallam <richardhallam25@gmail.com>
2024-12-12 12:21:04 +0000

    Hid secret_key

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -25,8 +25,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0w@73u2iffdkwlu6e57y(b3bxnj=ei7*$hg&3()#dg8)u7xfo&'
-#SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
