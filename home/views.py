e21ab9207b55215bd5ce2f7870ac82ca98a7f272
Richard Hallam <richardhallam25@gmail.com>
2024-12-12 14:15:17 +0000

    fixing errors shown by python linter

--- home/views.py
+++ home/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 
+
 def home_page(request):
     return render(request, 'home.html')
+
 
 def login(request):
     return render(request, 'login.html')
 
+
 def account_signup(request):
-    return render( request, 'signup.html')
+    return render(request, 'signup.html')
--- transactions/models.py
+++ transactions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Transaction(models.Model):
     transaction_id = models.AutoField(primary_key=True)
     transaction_name = models.CharField(max_length=255)
@@ -10,14 +11,18 @@ class Transaction(models.Model):
     account = models.ForeignKey('Account', on_delete=models.CASCADE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
+
     def __str__(self):
         return self.transaction_name
-    
+
+
 class Account(models.Model):
     account_id = models.AutoField(primary_key=True)
     account_name = models.CharField(max_length=255)
     account_value = models.FloatField(default=0.0)
 
+
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
     category_name = models.CharField(max_length=255)
+    
